404d7e6f7af1836ad39bbc8a757098232045821a
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2021-10-02 10:24:21 +0530

    Update automated whatsapp.py

--- automated whatsapp.py
+++ automated whatsapp.py
@@ -3,8 +3,10 @@ import time
 import pyautogui
 import keyboard
 kit.sendwhatmsg("+91XXXXXXXXXX","hello this is an automated msg",hh,mm,ss)
+#Fill the recieptiant's number in the Xes
+#Set the time in hh,mm,ss section (24 hour clock)
+
 pyautogui.press("enter")
 time.sleep(2)
 keyboard.press_and_release("enter")
 print("sent")
-
